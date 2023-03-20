f3f6cec15549c8f264192c8d310530b01095a05c
Prashant Prakash <97874555+Prashant-77@users.noreply.github.com>
2023-03-20 10:20:25 +0530

    Add files via upload

---
+++ Rock Paper Scissors Game.py
@@ -0,0 +1,34 @@
+''' This code is written by me
+Author= Prshant Prakash '''
+
+import random
+
+while True:
+    # get user input
+    user_choice = input("Enter Rock, Paper, or Scissors: ").lower()
+    
+    # validate user input
+    if user_choice not in ["rock", "paper", "scissors"]:
+        print("Invalid input. Please enter either Rock, Paper, or Scissors.")
+        continue
+    
+    # get computer's random choice
+    computer_choice = random.choice(["rock", "paper", "scissors"])
+    print("Computer chose", computer_choice)
+    
+    # determine the winner
+    if user_choice == computer_choice:
+        print("Tie!")
+    elif user_choice == "rock" and computer_choice == "scissors":
+        print("You win!")
+    elif user_choice == "paper" and computer_choice == "rock":
+        print("You win!")
+    elif user_choice == "scissors" and computer_choice == "paper":
+        print("You win!")
+    else:
+        print("Computer wins!")
+    
+    # ask user to play again
+    play_again = input("Do you want to play again? (yes/no)").lower()
+    if play_again != "yes":
+        break
